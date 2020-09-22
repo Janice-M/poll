4a5fdf08e4802a8129fabc0ff24193a0ec4aca74
Janice-M <janiceink001@gmail.com>
2020-09-22 23:43:05 +0300

    model

--- mypollsite/urls.py
+++ mypollsite/urls.py
@@ -13,9 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, path 
-from django.contrib import admin
-from . import views
+
 
 
 from django.contrib import admin
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,3 @@
-from django.db import models
+from django.db import mode
 
 # Create your models here.
