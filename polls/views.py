029a062e30fa75e2f841c9b7b7757a631fdad4a2
Janice-M <janiceink001@gmail.com>
2020-09-24 23:40:58 +0300

    index view edit

--- polls/views.py
+++ polls/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the polls inde
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
