c60a670b1f92ace202040443ffa49589344f2fc7
Janice-M <janiceink001@gmail.com>
2020-09-24 23:41:18 +0300

    index view edit

--- polls/views.py
+++ polls/views.py
@@ -5,7 +5,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls inde
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
