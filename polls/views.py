94744b71d982d21e886fe4974297c72480156a2e
Janice-M <janiceink001@gmail.com>
2020-09-24 23:41:29 +0300

    index view edit

--- polls/views.py
+++ polls/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    
+    output = ', '.join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
 
 def detail(request, question_id):
